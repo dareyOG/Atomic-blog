be8cf6d9a2f9caa8de190458065377ade895ac53
Oludire Damilare <oludire@gmail.com>
2024-09-26 20:54:37 +0100

    feat:  custom post_provider

--- src/App-v1.js
+++ src/App-v1.js
@@ -1,7 +1,5 @@
-import { createContext, useContext, useState } from 'react';
+import { createContext, useContext, useEffect, useState } from 'react';
 import { faker } from '@faker-js/faker';
-import useTheme from './useTheme';
-import { PostProvider, PostContext } from './PostContext';
 
 function createRandomPost() {
   return {
@@ -10,29 +8,67 @@ function createRandomPost() {
   };
 }
 
+const PostContext = createContext();
 const ThemeContext = createContext();
 
 function App() {
-  // custom useTheme hook
-  const [isFakeDark, setIsFakeDark] = useTheme('fake-dark-mode');
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost())
+  );
+  const [searchQuery, setSearchQuery] = useState('');
+  const [isFakeDark, setIsFakeDark] = useState(false);
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter(post =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase())
+        )
+      : posts;
+
+  function handleAddPost(post) {
+    setPosts(posts => [post, ...posts]);
+  }
+
+  function handleClearPosts() {
+    setPosts([]);
+  }
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle('fake-dark-mode');
+    },
+    [isFakeDark]
+  );
 
   return (
-    <section>
-      <ThemeContext.Provider value={{ isFakeDark, setIsFakeDark }}>
-        <ThemeToggler />
-      </ThemeContext.Provider>
-      <PostProvider>
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onAddPost: handleAddPost,
+        onClearPosts: handleClearPosts,
+        searchQuery,
+        setSearchQuery,
+      }}
+    >
+      <section>
+        <ThemeContext.Provider value={{ isFakeDark, setIsFakeDark }}>
+          <ThemeToggler />
+        </ThemeContext.Provider>
         <Header />
         <Main />
         <Archive />
         <Footer />
-      </PostProvider>
-    </section>
+      </section>
+    </PostContext.Provider>
   );
 }
 
 function ThemeToggler() {
-  const { isFakeDark, setIsFakeDark } = useContext(ThemeContext);
+  const { isFakeDark, setIsFakeDark } = useContext(DarkModeContext);
   return (
     <button
       onClick={() => setIsFakeDark(isFakeDark => !isFakeDark)}
